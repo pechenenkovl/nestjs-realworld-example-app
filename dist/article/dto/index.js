758540cccfa0c9f272c33c3d112b6e3dbac6c317
Vladyslav Pechenenko <Vladyslav.Pechenenko@3shape.com>
2026-05-18 22:26:49 +0300

    Changed to new DAL, MongoDb usage, new unit tests, updated to newest node.js

--- dist/article/dto/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var create_article_dto_1 = require("./create-article.dto");
-exports.CreateArticleDto = create_article_dto_1.CreateArticleDto;
-exports.CreateArticleRequestDto = create_article_dto_1.CreateArticleRequestDto;
-var create_comment_1 = require("./create-comment");
-exports.CreateCommentDto = create_comment_1.CreateCommentDto;
-exports.CreateCommentRequestDto = create_comment_1.CreateCommentRequestDto;
-//# sourceMappingURL=index.js.map
